179ee820d3609dfb04b21a122f8b27bebd9240d7
Siona Vivian <SionaSVivian@outlook.com>
2022-03-17 19:53:49 -0700

    FeaturedRow: modify styling

--- src/components/FeaturedRow.js
+++ src/components/FeaturedRow.js
@@ -8,34 +8,54 @@ import LinksContainer from "./styled/LinksContainer";
 const arrowSize = "25%";
 
 const Container = styled.li`
+  flex-direction: column;
   justify-content: center;
-  min-height: 290px;
+  min-height: fit-content;
   height: fit-content;
   margin-bottom: 32px;
-  padding: 25px 0;
+  padding: 10px 0;
   padding-left: 5px;
   overflow: hidden;
+
+  @media (min-width: 992px) {
+    flex-direction: row;
+    min-height: 290px;
+  }
 `;
 
 const PreviewContainer = styled.div`
+  align-self: center;
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 240px;
+  height: fit-content;
+  padding-bottom: 15px;
 
-  &:hover {
-    .arrows.featured,
-    .overlay {
-      display: block;
+  @media (min-width: 992px) {
+    &:hover {
+      .arrows.featured,
+      .overlay {
+        display: block;
+        cursor: pointer;
+      }
     }
+  }
+
+  @media (min-width: 992px) {
+    height: fit-content;
+    max-height: 260px;
+    padding-bottom: 0;
   }
 `;
 
 const PreviewWrapper = styled.div`
   position: relative;
+  display: flex;
+  align-items: center;
   max-width: fit-content;
   max-height: fit-content;
+  height: 100%;
 
   .arrows.featured {
     display: none;
@@ -52,10 +72,15 @@ const PreviewWrapper = styled.div`
 
 const PreviewImage = styled.img`
   max-width: 100%;
-  max-height: 100%;
+  max-height: unset;
+
+  @media (min-width: 993px) {
+    max-height: 260px;
+  }
 `;
 
 const LivePreview = styled.a`
+  width: fit-content;
   text-decoration: none;
 `;
 
@@ -71,22 +96,31 @@ const FeaturedRow = (props) => {
     <Container
       className={`row col-12 ${props.className ? props.className : ""}`}
     >
-      <PreviewContainer className="col-5 col-lg-4" onClick={toggleModal}>
+      <PreviewContainer
+        className="col-12 col-lg-6"
+        onClick={() => window.innerWidth >= 992 && toggleModal()}
+      >
         <PreviewWrapper>
           <PreviewImage src={props.previewImage} />
           <ArrowsFullscreen className="featured" />
           <div className="overlay"></div>
         </PreviewWrapper>
       </PreviewContainer>
-      <DescriptionContainer className="col-6 col-lg-6 ">
+      <DescriptionContainer className="col-12 col-lg-6">
         <h4>{props.title}</h4>
         <p>{props.description}</p>
         <LinksContainer className="row ">
-          <a className="github-preview" href={props.githubLink}></a>
+          <a
+            className="github-preview"
+            onClick={() => window.open(props.githubLink)}
+          ></a>
           {props.livePreviewLink ? (
             <React.Fragment>
               <div className="dot"></div>
-              <LivePreview href={props.livePreviewLink}>
+              <LivePreview
+                onClick={() => window.open(props.livePreviewLink)}
+                className="clickable"
+              >
                 Live Preview
               </LivePreview>
             </React.Fragment>
